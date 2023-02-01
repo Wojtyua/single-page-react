067d1e2f1bb676cf1a875163d6e06da41b48cd0e
Wojtyua <72139361+Wojtyua@users.noreply.github.com>
2023-02-01 15:20:44 +0100

    added swiper and testimonials section

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Services from './components/services/Services';
 import Qualification from './components/qualification/Qualification';
+import Testimonials from './components/testimonials/Testimonials';
 
 import './App.css';
 
@@ -17,6 +18,7 @@ function App() {
         <Skills />
         <Services />
         <Qualification />
+        <Testimonials />
       </main>
     </>
   );
